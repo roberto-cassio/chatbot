34a79e679971f928cf6770292d41fbdc6b2a50c6
roberto-cassio <rcslino99@gmail.com>
2025-10-15 20:04:32 -0300

    feat: added history of conversation to a more pleasant chatting experience

--- core/ai_clients.py
+++ core/ai_clients.py
@@ -3,21 +3,19 @@ from xai_sdk import Client
 from xai_sdk.chat import user, system
 
 class BaseAIClient:
-  def chat(self, system_message, user_message):
+  def chat(self, messages):
     raise NotImplementedError()
 
 class OpenAIClient(BaseAIClient):
     def __init__(self, api_key, model):
-        self.api_key = settings.OPENAI_API_KEY
+        self.api_key = api_key
         self.model = model
+        openai.api_key = self.api_key
 
-    def chat(self, system_message,user_message, temperature=0.7):
+    def chat(self, messages, temperature=0.7):
         response = openai.ChatCompletion.create(
             model=self.model,
-            messages=[
-                {"role": "system", "content": system_message},
-                {"role": "user", "content": user_message},
-            ],
+            messages=messages,
             temperature=temperature,
         )
         return response.choices[0].message['content']       
@@ -25,12 +23,13 @@ class OpenAIClient(BaseAIClient):
 class GrokClient(BaseAIClient):
       def __init__(self, api_key, model):
         self.model = model
-        self.api_key = settings.XAI_API_KEY
+        self.api_key = api_key
+        self.client = Client(api_key=self.api_key)
 
-      def chat(self, system_message, user_message):
-        client = Client(api_key=self.api_key)
-        chat = client.chat.create(model=self.api_key)
-        chat.append({"role": "system", "content": system_message})
-        chat.append({"role": "user", "content": user_message})
+
+      def chat(self, messages):
+        chat = self.client.chat.create(model=self.model)
+        for msg in messages:
+            chat.append(msg)
         response_data = chat.get_response()
         return response_data
--- core/services.py
+++ core/services.py
@@ -1,18 +1,22 @@
 import openai
 from django.conf import settings
 
+class ChatSession:
+  def __init__(self, system_message):
+    self.system_message = system_message
+    self.history = []
+
+  def add_user(self, message):
+    self.history.append({'role': 'user', 'content': message})
+  def add_bot(self, message):
+    self.history.append({'role': 'assistant', 'content': message})
+
+  def get_past_messages(self):
+      return [{'role': 'system', 'content': self.system_message}] + self.history
+
+
 class ChatBotService:
-  def __init__(self):
-        self.primary_client = OpenAIClient(
-          api_key=settings.OPENAI_API_KEY,
-          model=settings.OPENAI_MODEL
-        )
-        self.fallback_client = GrokClient(
-            api_key=settings.XAI_API_KEY,
-            model=settings.XAI_API_MODEL
-        )
-  def get_bot_response(self, user_message):
-    system = """Você é um assistente de vendas da Petlove, especializado em ajudar os usuários do e-commerce a encontrar e comprar produtos para seus animais de estimação. Seu objetivo é ser gentil, prestativo e eficiente em suas respostas, oferecendo uma experiência de compra personalizada.
+  system = """Você é um assistente de vendas da Petlove, especializado em ajudar os usuários do e-commerce a encontrar e comprar produtos para seus animais de estimação. Seu objetivo é ser gentil, prestativo e eficiente em suas respostas, oferecendo uma experiência de compra personalizada.
                   Mantenha um tom de conversa amigável, mas sem ser informal demais. Evite jargões ou gírias, mas também não seja excessivamente formal. Sempre que possível, forneça alternativas e opções claras para ajudar os usuários a tomar decisões mais rápidas e satisfatórias.
                   Você deve priorizar a clareza, objetividade e empatia. Seu foco é:
                   Empatia: Demonstrar compreensão e preocupação com o que o cliente está buscando para o seu pet.
@@ -30,12 +34,30 @@ class ChatBotService:
                   Sempre ser breve, clara e direta, sem perder a simpatia.
                   Nunca utilizar respostas que soem robóticas ou impessoais.
                   Aconselhe o cliente sempre que possível, oferecendo múltiplas opções e alternativas."""
-    return self._fallback_strategy(system, user_message)
+  
+  def __init__(self):
+    self.session = ChatSession(system=self.system)
+        self.primary_client = OpenAIClient(
+          api_key=settings.OPENAI_API_KEY,
+          model=settings.OPENAI_MODEL
+        )
+        self.fallback_client = GrokClient(
+            api_key=settings.XAI_API_KEY,
+            model=settings.XAI_API_MODEL
+        )
 
-  def _fallback_strategy(self, system_message, user_message):
+  def get_bot_response(self, user_message):
+    self.session.add_user(user_message)
+    messages = self.session.get_past_messages()
+    bot_response = self._fallback_strategy(messages)
+
+    self.session.add_bot(bot_response)
+    return bot_response
+
+  def _fallback_strategy(self, messages):
     try:
-        response = self.primary_client.chat(system_message, user_message, temperature=settings.OPENAI_TEMPERATURE)
-        return response.choices[0].message['content']
+        response = self.primary_client.chat(messages)
+        return response
     except Exception:
-        fallback_response = self.fallback_client.chat(system_message, user_message)
-        return fallback_response.get('response', 'Desculpe, não consegui responder agora.')
+        response = self.fallback_client.chat(messages)
+        return response
